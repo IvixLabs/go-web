f538f19b8bdf0a718ae3e9868e750defce2ca92b
Ivan Ruder <ivixmail@gmail.com>
2024-02-26 18:34:12 +0700

    Pack assets to one file

---
+++ api/web/frontapp/webpack.config.js
@@ -0,0 +1,17 @@
+const path = require('path');
+
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      }
+    ]
+  },
+  entry: './index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, '../static'),
+  },
+};
